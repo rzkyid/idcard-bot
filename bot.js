ec23789f1dba494040f563121665b5fd02b770fc
Nev1x <66782146+rzkyid@users.noreply.github.com>
2025-01-26 12:33:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,16 +43,16 @@ try {
     ctx.drawImage(template, 0, 0, canvas.width, canvas.height);
 
     // Gambar avatar
-    ctx.drawImage(avatar, 300, 100, 120, 150); // Posisi avatar
+    ctx.drawImage(avatar, 330, 100, 120, 120); // Posisi avatar
 
     // Gambar teks
-    drawText(ctx, `Nomor KTP: ${userId}`, 20, 100, { font: '18px Arial', color: '#000000' });
-    drawText(ctx, `Nama: ${nama}`, 20, 120, { font: '18px Arial', color: '#000000' });
+    drawText(ctx, `Nomor KTP: ${userId}`, 20, 80, { font: '18px Arial', color: '#000000' });
+    drawText(ctx, `Nama: ${nama}`, 20, 110, { font: '18px Arial', color: '#000000' });
     drawText(ctx, `Jenis Kelamin: ${gender}`, 20, 140, { font: '18px Arial', color: '#000000' });
-    drawText(ctx, `Domisili: ${domisili}`, 20, 160, { font: '18px Arial', color: '#000000' });
-    drawText(ctx, `Agama: ${agama}`, 20, 180, { font: '18px Arial', color: '#000000' });
-    drawText(ctx, `Hobi: ${hobi}`, 20, 200, { font: '18px Arial', color: '#000000' });
-    drawText(ctx, `Tanggal: ${createdAt}`, 300, 200, { font: '12px Arial', color: '#000000' });
+    drawText(ctx, `Domisili: ${domisili}`, 20, 170, { font: '18px Arial', color: '#000000' });
+    drawText(ctx, `Agama: ${agama}`, 20, 200, { font: '18px Arial', color: '#000000' });
+    drawText(ctx, `Hobi: ${hobi}`, 20, 230, { font: '18px Arial', color: '#000000' });
+    drawText(ctx, `Tanggal: ${createdAt}`, 350, 230, { font: '12px Arial', color: '#000000' });
 
     return canvas.toBuffer('image/png');
 } catch (error) {
